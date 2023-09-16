56d434ca6ecea5ae5d4ad4698ba32b16665e8208
Minal Sonawane <85930628+minal589@users.noreply.github.com>
2023-09-16 07:30:31 +0000

    container config done

--- 1_Basics/P2.js
+++ 1_Basics/P2.js
@@ -7,3 +7,11 @@ var city = "Mumbai";
 // id = 10; // Not allowed
 
 console.table([id, email, city]); // print one or more value 
+
+// Datatypes
+// number ==> 2 to power 53
+// bigint
+// string ==> " "
+// boolean ==> true/false
+// null ==> standalone file
+//  undefined ==> 
