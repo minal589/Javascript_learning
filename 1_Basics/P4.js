ad7c97f9a5d9380f82fd6ff06c30b23e9d0609d6
Minal Sonawane <85930628+minal589@users.noreply.github.com>
2023-09-16 12:18:15 +0000

    container config done

---
+++ 1_Basics/P4.js
@@ -0,0 +1,11 @@
+// String
+
+let name = "Rahul";
+let age = 25;
+
+console.log(`Hello my name is ${name} and age is ${age}`);
+
+const Name = new String('Rahul')
+console.log(Name[0]);
+console.log(Name.toLocaleUpperCase());
+console.log(Name.length);
