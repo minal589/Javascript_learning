525b16ed5b2ef4dbc9ded4950210a21cc32ad7ec
Minal Sonawane <85930628+minal589@users.noreply.github.com>
2023-09-16 12:19:52 +0000

    container config done

---
+++ 1_Basics/P3.js
@@ -0,0 +1,11 @@
+console.log("1" + 2)
+console.log(1 + "2");
+console.log(1 + 2 + "2")
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2;
+console.table([num1, num2, num3]);
+
+let x = 3;
+const y = x++;
+console.log(x + " " + y);
