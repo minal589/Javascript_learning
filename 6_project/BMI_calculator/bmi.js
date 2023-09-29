ecbea3d923025fc54f7789d0299bbb9965e7f3ad
Minal Sonawane <85930628+minal589@users.noreply.github.com>
2023-09-29 06:20:16 +0000

    BMI Calculator

---
+++ 6_project/BMI_calculator/bmi.js
@@ -0,0 +1,31 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const result = document.querySelector('#results');
+
+    if(height === '' || height<0 || isNaN(height)){
+        result.innerHTML = `please give valid height ${height}`;
+    }
+    else if(weight === '' || weight<0 || isNaN(weight)){
+        result.innerHTML = `please give valid weight ${weight}`;
+    }
+    else{
+        let bmi = (weight / ((height*height)/10000)).toFixed(2);
+        
+    
+        if(bmi>18.6 && bmi<24.9){
+            result.innerHTML = `<span> Noraml Range : ${bmi}</span>`;
+        }
+        else if(bmi < 18.6){
+            result.textContent = `Under weight:${bmi}`;
+        }
+        else{
+            result.innerHTML = `Overweight : ${bmi}`;
+        }
+}
+
+})
